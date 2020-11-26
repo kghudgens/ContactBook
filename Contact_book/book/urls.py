e132f14eb7728b7770680db7770a4d419679ff47
Kevin Hudgens <hudgens1073@gmail.com>
2020-11-26 21:54:50 +0900

    Edited the index view to associate with the newly created class view

--- Contact_book/book/urls.py
+++ Contact_book/book/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='book-index'),
+    path('', views.ContactListView.as_view(), name='book-index'),
     path('book_form/', views.book_form, name='book-book_form'),
     path('about/', views.about, name='about' )
 ]

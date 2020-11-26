be43e6ef480d6832bc426574df13fcde7f6f9829
Kevin Hudgens <hudgens1073@gmail.com>
2020-11-26 21:53:17 +0900

    Added the class based list view to add data to home page. In the class, mentioned it was to be associated with the Contact book model and referenced the template to be the home page.

--- Contact_book/book/views.py
+++ Contact_book/book/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import ContactForm
 from django.contrib import messages
+from django.views import generic
+from .models import ContactBook
+from .forms import ContactForm
 # Create your views here.
 
 def index(request):
     
     return render(request, 'book/index.html', )
+
+class ContactListView(generic.ListView):
+    model = ContactBook
+    template_name = 'book/index.html'
 
 def book_form(request):
     if request.method=="POST":

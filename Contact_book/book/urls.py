4f1c1c43df1306fb383ce5529459bbd41adefb1e
Kevin Hudgens <hudgens1073@gmail.com>
2020-11-29 11:57:27 +0900

    Added the views for the Detail View

--- Contact_book/book/urls.py
+++ Contact_book/book/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.ContactListView.as_view(), name='book-index'),
+    path(
+        'contact/<int:pk>', 
+        views.ContactDetailView.as_view(), 
+        name='contact_detail'
+        ),
     path('book_form/', views.book_form, name='book-book_form'),
     path('about/', views.about, name='about' )
 ]
